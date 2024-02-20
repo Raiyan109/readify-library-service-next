686fba1e710f24cbaf207ea34b3a57b4557b7f94
Raiyan109 <kraiyan109@gmail.com>
2024-02-20 15:46:22 +0600

    db connected with signup route, hashedpassword, successfully stored user in mongodb

--- app/api/signup/route.js
+++ app/api/signup/route.js
@@ -1,10 +1,14 @@
+import { connectDB } from "@/lib/mongodb"
+import User from "@/models/userModel"
 import { NextResponse } from "next/server"
+import bcrypt from 'bcrypt'
 
 export async function POST(req) {
     try {
         const { name, email, password } = await req.json()
-
-        console.log(name, email, password);
+        const hashedPassword = await bcrypt.hash(password, 10)
+        await connectDB()
+        await User.create({ name, email, password: hashedPassword })
 
         return NextResponse.json({ message: 'User signed up successfully' })
     } catch (error) {
