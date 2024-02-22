acbf5f1d4b7a7bdada0c8146d89ccb7cf8f9d086
Raiyan109 <kraiyan109@gmail.com>
2024-02-22 16:12:07 +0600

    edit page, edit route by,now edit route on server side api folder

--- app/components/Books.js
+++ app/components/Books.js
@@ -1,11 +1,20 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export const Books = ({ book }) => {
     return (
-        <div className='glass flex flex-col justify-center items-center p-5 rounded-xl'>
-            <Image src={book.cover} width={100} height={100} style={{ objectFit: 'cover' }} />
-            <h1>{book.name}</h1>
+        <div>
+            {/* <Link href={`/${}`}> */}
+            <div className='glass flex flex-col justify-center items-center p-5 rounded-xl space-y-9'>
+                <Image src={book.cover} width={100} height={100} style={{ objectFit: 'cover' }} />
+                <h1>{book.name}</h1>
+                <div className='flex items-center gap-5'>
+                    <Link href={`/editBook/${book?._id}`}><button className="btn btn-xs btn-primary">Edit</button></Link>
+                    <button className="btn btn-xs btn-accent">Delete</button>
+                </div>
+            </div>
+            {/* </Link> */}
         </div>
     )
 }
---
+++ app/editBook/[id]/page.js
@@ -0,0 +1,40 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+
+
+export default function EditBook({ params }) {
+    const [name, setName] = useState('')
+    const [file, setFile] = useState(null)
+    const [error, setError] = useState('')
+    const router = useRouter()
+    const { id } = params
+    console.log(id);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+
+    }
+    return (
+        <div className="p-12 flex justify-center items-center">
+            <div className="border-primary border p-20">
+                <h1 className="text-center text-2xl font-bold mb-12">Edit Book</h1>
+                <form onSubmit={handleSubmit} className="space-y-8">
+
+                    <div>
+                        <input type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Book Name" className="input input-bordered input-primary w-full max-w-xs" />
+                    </div>
+                    <div>
+                        <button className="btn btn-primary w-full text-lg" type="submit">Edit</button>
+                    </div>
+                </form>
+                {error && <p className="text-accent">{error}</p>}
+            </div>
+        </div>
+    )
+}
