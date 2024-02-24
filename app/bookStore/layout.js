ee34c4cf29e0217111f5cfc1afd1b2503a821e36
Raiyan109 <kraiyan109@gmail.com>
2024-02-24 10:36:09 +0600

    book store hero

--- app/bookStore/layout.js
+++ app/bookStore/layout.js
@@ -19,13 +19,24 @@ export default function BookStoreLayout({ children }) {
                     </div>
                     <div className="drawer-side">
                         <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-                        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                        <div className="menu p-4 w-80 min-h-full bg-base-200 text-base-content gap-2">
                             {/* Sidebar content here */}
                             <h1 className="p-5 text-2xl font-bold">Dashboard</h1>
                             <Link href='/addBook'>
                                 <div className="bg-secondary hover:bg-accent text-black font-semibold text-lg rounded-lg w-full px-2 py-3">Add New Book</div>
                             </Link>
-                        </ul>
+                            <a href="#category" className="bg-secondary hover:bg-accent text-black font-semibold text-lg rounded-lg w-full px-2 py-3">
+                                Category
+                            </a>
+                            <a href="#category" className="bg-secondary hover:bg-accent text-black font-semibold text-lg rounded-lg w-full px-2 py-3">
+                                My Library
+                            </a>
+                            <a href="#category" className="bg-secondary hover:bg-accent text-black font-semibold text-lg rounded-lg w-full px-2 py-3">
+                                Download                            </a>
+                            <a href="#category" className="bg-secondary hover:bg-accent text-black font-semibold text-lg rounded-lg w-full px-2 py-3">
+                                Favorite
+                            </a>
+                        </div>
                     </div>
                 </div>
                 <div className="p-8">
--- app/bookStore/page.js
+++ app/bookStore/page.js
@@ -1,10 +1,21 @@
+import Image from 'next/image'
 import { BookStoreHome } from '../components/BookStoreHome'
+import openBook from '@/public/images/open-book.jpg'
 
 export default async function BookStore() {
-
-
-
     return (
-        <BookStoreHome />
+        <div>
+            <div className="hero bg-base-200 shadow-xl gap-6">
+                <div className="hero-content flex-col lg:flex-row-reverse gap-36">
+                    <Image src={openBook} alt='Book' width={300} height={300} style={{ objectFit: 'cover' }} />
+                    <div>
+                        <h1 className="text-5xl font-bold">Hello, Reader! </h1>
+                        <p className="py-6">Selection of the best books, just for you</p>
+                        <button className="btn btn-primary">Show Latest</button>
+                    </div>
+                </div>
+            </div>
+            <BookStoreHome />
+        </div>
     )
 }
