af6a1a18c283e3df2d2573c06b1767c956416c86
Raiyan109 <kraiyan109@gmail.com>
2024-02-25 15:58:26 +0600

    favicon added

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,5 +1,5 @@
 "use client"
-import logo from '@/public/images/readify-logo.png'
+import logo from '@/public/images/logo.png'
 import Image from 'next/image'
 import { ThemeContext } from '@/context/ThemeContext'
 import { useContext } from 'react'
@@ -9,10 +9,10 @@ export const Navbar = () => {
     const { changeTheme } = useContext(ThemeContext);
     return (
         <div>
-            <div className="navbar bg-base-100 flex justify-between">
+            <div className="navbar bg-base-100 flex justify-between overflow-hidden">
                 <a className="text-xl flex flex-col justify-start items-start">
-                    <Image src={logo} height={90} width={90} alt='logo' />
-                    <p className='text-primary text-xs'>Library Services</p>
+                    <Image src={logo} height={80} width={80} alt='logo' />
+
                 </a>
                 <div className=" ">
                     <ThemeBtn handleOnClick={changeTheme} />
--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import { Hero } from "./components/Hero";
 
 
 export default function Home() {
   return (
     <main className="">
+      <Head>
+        <link rel="icon" href="/public/images/favicon.ico" sizes="any" />
+      </Head>
       <Hero />
     </main>
   );
